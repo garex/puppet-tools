3680e00bed74aa88a2193743a0ca10d5a879e81d
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2012-08-24 22:48:28 +0700

    Expand all command switches to full notation

--- apply.sh
+++ apply.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
+modulePath='~/puppet'
 
 echo "Getting settings"
 settingsPath=$1
 . $settingsPath
 
+# zero
 if [ -z "$puppetColor" ]; then
   puppetColor="false"
 fi
 
 echo "Uploading puppet"
 ssh $userName@$hostName "
-  rm -rf ~/puppet;
-  mkdir -p ~/puppet;
+  rm --recursive --force $modulePath;
+  mkdir --parents $modulePath;
 "
 for module in $(echo $modules | tr ":" "\n")
 do
-  scp -r -q $module $userName@$hostName:~/puppet/$(basename $module)
+  # recursively, quiet & with compression
+  scp -r -q -C $module $userName@$hostName:$modulePath/$(basename $module)
 done
 
 echo "Applying puppet"
 ssh $userName@$hostName "
-  sudo puppet apply --verbose --color $puppetColor --modulepath ~/puppet ~/puppet/$entryPoint;
-  rm -rf ~/puppet;
+  sudo puppet apply --verbose --color $puppetColor --modulepath $modulePath $modulePath/$entryPoint;
+  rm --recursive --force $modulePath;
 "
 echo "Done"
